eb8c2d8fcd52f0a1ed9d8e5f7b5fa1dadf70afd5
Douglas <douglas.e.mckinley@gmail.com>
2024-09-04 15:41:01 -0700

    Update setup_selenium.sh

--- setup_selenium.sh
+++ setup_selenium.sh
@@ -9,26 +9,26 @@ sudo yum install -y fontconfig freetype freetype-devel alsa-lib atk cairo \
     cups-libs dbus-glib GConf2 libX11 libXcomposite libXcursor libXdamage \
     libXext libXi libXrandr libXScrnSaver libXtst pango
 
-# Download and install Google Chrome
+# Remove any existing Chrome installation
+sudo yum remove -y google-chrome-stable
+
+# Download and install the latest stable version of Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 sudo yum install -y ./google-chrome-stable_current_x86_64.rpm
 
 # Verify Chrome installation
 google-chrome --version
 
-# Get Chrome version
-CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+' | head -n1)
-
-# Download the matching ChromeDriver version
-wget -N https://chromedriver.storage.googleapis.com/$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)/chromedriver_linux64.zip
+# Download the latest stable version of ChromeDriver from the new source
+wget -N https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/115.0.5790.170/linux64/chromedriver-linux64.zip
 
 # Unzip and move ChromeDriver to /usr/local/bin
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/local/bin/
+unzip chromedriver-linux64.zip
+sudo mv chromedriver-linux64/chromedriver /usr/local/bin/
 sudo chmod +x /usr/local/bin/chromedriver
 
 # Clean up
-rm chromedriver_linux64.zip
+rm -rf chromedriver-linux64.zip chromedriver-linux64
 
 # Install pip and Selenium
 sudo yum install -y python3-pip
