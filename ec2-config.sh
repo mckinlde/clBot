d2f9a995e83c83c9c1c6f723b85212e93408a4fe
“Douglas” <douglas.e.mckinley@gmail.com>
2024-09-04 11:27:05 -0700

    install node on ec2

--- ec2-config.sh
+++ ec2-config.sh
@@ -11,3 +11,14 @@ ssh-keygen -t ed25519 -C "douglas.e.mckinley@gmail.com"
 # clone the repo
 cat ~/ec2-ssh-key.pub >> ~/.ssh/authorized_keys
 git clone git@github.com:mckinlde/clBot.git
+
+# Installing Node 
+#   https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# load nvm
+source ~/.bashrc
+# use nvm to install latest Node.js LTS (this includes NPM)
+nvm install --lts
+# test that node is installed
+node -e "console.log('Running Node.js ' + process.version)"
