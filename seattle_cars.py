11bd6b837e967884aa2e12d469ce2b9870e1557d
Douglas <douglas.e.mckinley@gmail.com>
2024-09-04 20:50:13 -0700

    it appears to be simply working

--- seattle_cars.py
+++ seattle_cars.py
@@ -8,9 +8,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# for writing to dynamoDB
+# for reading/writing to dynamoDB
 import boto3
-from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Key, Attr
 # for sending emails
 import smtplib
 from email.mime.text import MIMEText
@@ -35,17 +35,67 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 # Initialize a session using Amazon DynamoDB
 session = boto3.Session(region_name='us-west-2')  # Replace with your region
 dynamodb = session.resource('dynamodb')
-
 # Select your DynamoDB table
 table = dynamodb.Table('listings')  # Replace with your table name
 
 # Set your CL area
-area = 'seattle'
+areas = [
+    "auburn", "bham", "dothan", "shoals", "gadsden", "huntsville", "mobile", "montgomery", "tuscaloosa",
+    "anchorage", "fairbanks", "kenai", "juneau", "flagstaff", "mohave", "phoenix", "prescott", "showlow",
+    "sierravista", "tucson", "yuma", "fayar", "fortsmith", "jonesboro", "littlerock", "texarkana",
+    "bakersfield", "chico", "fresno", "goldcountry", "hanford", "humboldt", "imperial", "inlandempire",
+    "losangeles", "mendocino", "merced", "modesto", "monterey", "orangecounty", "palmsprings", "redding",
+    "sacramento", "sandiego", "sfbay", "slo", "santabarbara", "santamaria", "siskiyou", "stockton",
+    "susanville", "ventura", "visalia", "yubasutter", "boulder", "cosprings", "denver", "eastco",
+    "fortcollins", "rockies", "pueblo", "westslope", "newlondon", "hartford", "newhaven", "nwct",
+    "delaware", "washingtondc", "miami", "daytona", "keys", "miami", "fortmyers", "gainesville", "cfl",
+    "jacksonville", "lakeland", "miami", "lakecity", "ocala", "okaloosa", "orlando", "panamacity",
+    "pensacola", "sarasota", "miami", "spacecoast", "staugustine", "tallahassee", "tampa", "treasure",
+    "miami", "albanyga", "athensga", "atlanta", "augusta", "brunswick", "columbusga", "macon", "nwga",
+    "savannah", "statesboro", "valdosta", "honolulu", "boise", "eastidaho", "lewiston", "twinfalls",
+    "bn", "chambana", "chicago", "decatur", "lasalle", "mattoon", "peoria", "rockford", "carbondale",
+    "springfieldil", "quincy", "bloomington", "evansville", "fortwayne", "indianapolis", "kokomo",
+    "tippecanoe", "muncie", "richmondin", "southbend", "terrehaute", "ames", "cedarrapids", "desmoines",
+    "dubuque", "fortdodge", "iowacity", "masoncity", "quadcities", "siouxcity", "ottumwa", "waterloo",
+    "lawrence", "ksu", "nwks", "salina", "seks", "swks", "topeka", "wichita", "bgky", "eastky",
+    "lexington", "louisville", "owensboro", "westky", "batonrouge", "cenla", "houma", "lafayette",
+    "lakecharles", "monroe", "neworleans", "shreveport", "maine", "annapolis", "baltimore", "easternshore",
+    "frederick", "smd", "westmd", "boston", "capecod", "southcoast", "westernmass", "worcester",
+    "annarbor", "battlecreek", "centralmich", "detroit", "flint", "grandrapids", "holland", "jxn",
+    "kalamazoo", "lansing", "monroemi", "muskegon", "nmi", "porthuron", "saginaw", "swmi", "thumb", "up",
+    "bemidji", "brainerd", "duluth", "mankato", "minneapolis", "rmn", "marshall", "stcloud", "gulfport",
+    "hattiesburg", "jackson", "meridian", "northmiss", "natchez", "columbiamo", "joplin", "kansascity",
+    "kirksville", "loz", "semo", "springfield", "stjoseph", "stlouis", "billings", "bozeman", "butte",
+    "greatfalls", "helena", "kalispell", "missoula", "montana", "grandisland", "lincoln", "northplatte",
+    "omaha", "scottsbluff", "elko", "lasvegas", "reno", "nh", "cnj", "jerseyshore", "newjersey",
+    "southjersey", "albuquerque", "clovis", "farmington", "lascruces", "roswell", "santafe", "albany",
+    "binghamton", "buffalo", "catskills", "chautauqua", "elmira", "fingerlakes", "glensfalls",
+    "hudsonvalley", "ithaca", "longisland", "newyork", "oneonta", "plattsburgh", "potsdam", "rochester",
+    "syracuse", "twintiers", "utica", "watertown", "asheville", "boone", "charlotte", "eastnc",
+    "fayetteville", "greensboro", "hickory", "onslow", "outerbanks", "raleigh", "wilmington",
+    "winstonsalem", "bismarck", "fargo", "grandforks", "nd", "akroncanton", "ashtabula", "athensohio",
+    "chillicothe", "cincinnati", "cleveland", "columbus", "dayton", "limaohio", "mansfield", "sandusky",
+    "toledo", "tuscarawas", "youngstown", "zanesville", "lawton", "enid", "oklahomacity", "stillwater",
+    "tulsa", "bend", "corvallis", "eastoregon", "eugene", "klamath", "medford", "oregoncoast", "portland",
+    "roseburg", "salem", "altoona", "chambersburg", "erie", "harrisburg", "lancaster", "allentown",
+    "meadville", "philadelphia", "pittsburgh", "poconos", "reading", "scranton", "pennstate",
+    "williamsport", "york", "providence", "charleston", "columbia", "florencesc", "greenville",
+    "hiltonhead", "myrtlebeach", "nesd", "csd", "rapidcity", "siouxfalls", "sd", "chattanooga",
+    "clarksville", "cookeville", "jacksontn", "knoxville", "memphis", "nashville", "tricities",
+    "abilene", "amarillo", "austin", "beaumont", "brownsville", "collegestation", "corpuschristi",
+    "dallas", "nacogdoches", "delrio", "elpaso", "galveston", "houston", "killeen", "laredo",
+    "lubbock", "mcallen", "odessa", "sanangelo", "sanantonio", "sanmarcos", "bigbend", "texoma",
+    "easttexas", "victoriatx", "waco", "wichitafalls", "logan", "ogden", "provo", "saltlakecity",
+    "stgeorge", "vermont", "charlottesville", "danville", "fredericksburg", "norfolk", "harrisonburg",
+    "lynchburg", "blacksburg", "richmond", "roanoke", "swva", "winchester", "bellingham", "kpr",
+    "moseslake", "olympic", "pullman", "seattle", "skagit", "spokane", "wenatchee", "yakima",
+    "charlestonwv", "martinsburg", "huntington", "morgantown", "wheeling", "parkersburg", "swv", "wv",
+    "appleton", "eauclaire", "greenbay", "janesville", "racine", "lacrosse", "madison", "milwaukee",
+    "northernwi", "sheboygan", "wausau", "wyoming", "micronesia", "puertorico", "virgin"
+]
 
 
 # -----------------------------------------------------------------------------
-
-
 # HELPER FUNCTIONS
 # -----------------------------------------------------------------------------
 def get_soup_from_url(driver, url, timeout=10):
@@ -155,62 +205,147 @@ def soup_from_html(page_html):
     soup = BeautifulSoup(page_html, 'html.parser')
     return soup
 
-# -----------------------------------------------------------------------------
 
+def query_dynamodb(area_value):
+    # Query the DynamoDB table for items with a specific area and filter by activity
+    response = table.query(
+        KeyConditionExpression=Key('area').eq(area_value),
+        FilterExpression=Attr('activity').eq('active')
+    )
+
+    items = response.get('Items', [])
+
+    # Process the results
+    result_dict = {}
+    for item in items:
+        url = item.get('url')
+        if url:  # Ensure that 'url' is present in the item
+            result_dict[url] = {
+                'activity': item.get('activity'),
+                'listing_html': item.get('listing_html'),
+                'updated': item.get('updated')
+            }
+
+    return result_dict
+
+
+def process_frontpage(area, driver, get_soup_from_url, extract_frontpage_links, get_html_from_url, soup_from_html, check_listing_activity):
+    # Fetch the front page soup from the URL
+    frontpage_soup = get_soup_from_url(
+        driver,
+        f'https://{area}.craigslist.org/search/cta?purveyor=owner#search=1~gallery~0~0'
+    )
+
+    # Extract links from the front page
+    links = extract_frontpage_links(frontpage_soup)
+
+    new_listings_count = 0
+
+    for i, link in enumerate(links, start=1):
+        # Get HTML content from the link
+        link_html = get_html_from_url(driver, link)
+
+        # Check listing activity
+        listing_activity = check_listing_activity(soup_from_html(link_html))
+
+        if listing_activity['is_expected']:
+            # Insert new item into DynamoDB
+            table.put_item(
+                Item={
+                    'area': area,  # primary key (partition key)
+                    'url': link,  # Sort key
+                    'added': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
+                    'activity': 'active',
+                    'updated': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
+                    'listing_html': link_html
+                }
+            )
+            new_listings_count += 1
+
+    return {'new_listings_added': new_listings_count}
+
+
+def update_existing_listings(existing_listings, area, driver, get_html_from_url, soup_from_html, check_listing_activity):
+    listings_updated_count = 0
+    listings_checked_count = 0
+
+    for i, link in enumerate(existing_listings, start=1):
+        # Get HTML content from the link
+        link_html = get_html_from_url(driver, link)
+
+        # Check listing activity
+        listing_activity = check_listing_activity(soup_from_html(link_html))
+
+        listings_checked_count += 1
+
+        if not listing_activity['is_expected']:
+            # Update existing item in DynamoDB
+            table.update_item(
+                Key={"area": area, "url": link},
+                UpdateExpression="set activity=:s, updated=:u",
+                ExpressionAttributeValues={
+                    ":s": listing_activity['activity'],
+                    ":u": datetime.datetime.now().isoformat()
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            listings_updated_count += 1
+
+    return {
+        'listings_updated': listings_updated_count,
+        'listings_checked': listings_checked_count
+    }
+
+
+# -----------------------------------------------------------------------------
 # LOOP
 # -----------------------------------------------------------------------------
 
+startTime_of_country = datetime.datetime.now()
 
-# Example of querying items
-response = table.query(
-    KeyConditionExpression=Key('area').eq(area)  # Replace with your key condition
-)
+for area in areas:
 
-items = response['Items']
+    startTime_of_area = datetime.datetime.now()
 
-for item in items:
-    print(item)
-    #print(check_listing_activity(soup_from_html(item)))
+    # Query the table
+    existing_listings = query_dynamodb(area)
 
-frontpage_soup = (
-    get_soup_from_url(driver,
-                      'https://{area}.craigslist.org/search/cta?purveyor=owner#search=1~gallery~0~0'.format(area=area)
-                      )
-)
-links = extract_frontpage_links(frontpage_soup)
+    # 1. Check existing listings
+    updated = update_existing_listings(
+        existing_listings,
+        area,
+        driver,
+        get_html_from_url,
+        soup_from_html,
+        check_listing_activity
+    )
+    # 2. add anything from frontpage
+    processed = process_frontpage(
+        area,
+        driver,
+        get_soup_from_url,
+        extract_frontpage_links,
+        get_html_from_url,
+        soup_from_html,
+        check_listing_activity
+    )
 
-i = 0
-for link in links:
-    i = i + 1
-    print('link: ', link)
-    print(i, ' out of ', len(links))
-    link_html = get_html_from_url(driver, link)
-    listing_activity = check_listing_activity(soup_from_html(link_html))
-    if listing_activity['is_expected']:
-        table.put_item(
-            Item={
-                'area': 'seattle',  # primary key (partition key)
-                'url': link,  # Sort key
-                'added': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
-                'activity': 'active',
-                'updated': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
-                'listing_html': link_html
-            }
-        )
-    else:
-        table.update_item(
-                Key={"area": area, "url": link},
-                UpdateExpression="set activity=:s, updated=:u",
-                ExpressionAttributeValues={":s": listing_activity['activity'], ":u": datetime.datetime.now().isoformat()},
-                ReturnValues="UPDATED_NEW",
-            )
+    endTime_of_area = datetime.datetime.now()
 
+    send_mail('douglasemckinley@gmail.com', 'johnnygooddeals@gmail.com',
+              'ec2 {area} finished, updated: {updated}, processed: {processed}, timeDelta: {areaDeltaT}'.format(
+                  area, updated, processed, (startTime_of_area-endTime_of_area)),
+              'ec2 {area} finished, updated: {updated}, processed: {processed}, timeDelta: {areaDeltaT}'.format(
+                  area, updated, processed, (startTime_of_area-endTime_of_area))
+              )
+
+endTime_of_country = datetime.datetime.now()
 
 driver.quit()
 
 send_mail('douglasemckinley@gmail.com', 'johnnygooddeals@gmail.com',
-          'ec2 finished',
-          'links checked: {length: .2f}'.format(length=len(links)))
+          'ec2 all areas finished, timeDelta: {countryDeltaT}'.format(startTime_of_country-endTime_of_country),
+          'wahoo')
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
