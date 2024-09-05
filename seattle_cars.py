7a991662804ac1f2f39a89e27adf2ed8a9cd495e
Douglas <douglas.e.mckinley@gmail.com>
2024-09-04 19:16:46 -0700

    check for updates, only write if not updateable

--- seattle_cars.py
+++ seattle_cars.py
@@ -38,6 +38,11 @@ dynamodb = session.resource('dynamodb')
 
 # Select your DynamoDB table
 table = dynamodb.Table('cars')  # Replace with your table name
+
+# Set your CL area
+area = 'seattle'
+
+
 # -----------------------------------------------------------------------------
 
 
@@ -88,6 +93,11 @@ def write_soup_to_file(soup, file_name):
         f.write(soup.prettify())
 
 
+def write_html_to_file(html, file_name):
+    with open('html_captures/' + file_name, "w") as f:
+        f.write(html)
+
+
 def extract_frontpage_links(bs4_object):
     links = []
     for link in bs4_object.find_all('a', class_='posting-title'):
@@ -114,46 +124,93 @@ def send_mail(to, from_addr, subject, text):
     # s.sendmail(from_addr, [to], msg.as_string())
     s.quit()
 
+
+def check_listing_status(soup):
+    # Check if the listing is as expected
+    if soup.find("span", attrs={"class": "postingtitletext"}) is not None:
+        return {'is_expected': True, 'status': 'listing as expected'}
+
+    # Check if the listing has been removed by the author
+    if 'this posting has been deleted by its author' in soup.get_text().lower():
+        return {'is_expected': False, 'status': 'removed by author'}
+
+    # Check if the listing has been flagged
+    elif 'this posting has been flagged for removal' in soup.get_text().lower():
+        return {'is_expected': False, 'status': 'listing flagged'}
+
+    # Check if the listing has 404'd
+    elif '404 error' in soup.get_text().lower():
+        return {'is_expected': False, 'status': 'listing 404'}
+
+    # Check if the listing has expired
+    if 'this posting has expired' in soup.get_text().lower():
+        return {'is_expected': False, 'status': 'expired'}
+
+    # If none of the above cases match
+    return {'is_expected': False, 'status': 'unknown'}
+
+
+def soup_from_html(page_html):
+    # Create BeautifulSoup soup object
+    soup = BeautifulSoup(page_html, 'html.parser')
+    return soup
+
 # -----------------------------------------------------------------------------
 
 # LOOP
 # -----------------------------------------------------------------------------
 
+
 # Example of querying items
 response = table.query(
-    KeyConditionExpression=Key('url').eq('example')  # Replace with your key condition
+    KeyConditionExpression=Key('area').eq(area)  # Replace with your key condition
 )
 
 items = response['Items']
 
 for item in items:
     print(item)
+    #print(check_listing_status(soup_from_html(item)))
 
-frontpage_soup = get_soup_from_url(driver, 'https://seattle.craigslist.org/search/cta?purveyor=owner#search=1~gallery~0~0')
+frontpage_soup = (
+    get_soup_from_url(driver,
+                      'https://{area}.craigslist.org/search/cta?purveyor=owner#search=1~gallery~0~0'.format(area=area)
+                      )
+)
 links = extract_frontpage_links(frontpage_soup)
 
 i = 0
 for link in links:
-    i=i+1
+    i = i + 1
     print('link: ', link)
     print(i, ' out of ', len(links))
     link_html = get_html_from_url(driver, link)
-    table.put_item(
-    Item={
-        'url': link,  # primary key (partition key)
-        'area': 'seattle', # Sort key
-        'added': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
-        'status': 'active',
-        'updated': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
-        'listing_html': link_html
-    }
-)
+    listing_status = check_listing_status(soup_from_html(link_html))
+    if listing_status['is_expected']:
+        table.put_item(
+            Item={
+                'url': link,  # primary key (partition key)
+                'area': 'seattle',  # Sort key
+                'added': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
+                'status': 'active',
+                'updated': datetime.datetime.now().isoformat(),  # Convert to ISO 8601 string
+                'listing_html': link_html
+            }
+        )
+    else:
+        table.update_item(
+                Key={"url": link, "area": area},
+                UpdateExpression="set status=:s, updated=:u",
+                ExpressionAttributeValues={":s": listing_status['status'], ":u": datetime.datetime.now().isoformat()},
+                ReturnValues="UPDATED_NEW",
+            )
+
 
 driver.quit()
 
 send_mail('douglasemckinley@gmail.com', 'johnnygooddeals@gmail.com',
           'ec2 finished',
-          'links checked: {length: .2f}'.format(length = len(links)))
+          'links checked: {length: .2f}'.format(length=len(links)))
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
