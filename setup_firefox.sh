2536aea42a19de090e1b31e0444cf2d886e35246
Douglas <douglas.e.mckinley@gmail.com>
2024-09-07 17:03:42 -0700

    Update setup_firefox.sh

--- setup_firefox.sh
+++ setup_firefox.sh
@@ -2,5 +2,9 @@ wget "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US
 tar xjf firefox-latest.tar.bz2
 sudo mv firefox /opt/firefox
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+sudo yum install -y mesa-libGL
+export MOZ_HEADLESS=1
+sudo yum install -y xorg-x11-server-Xvfb
+Xvfb :99 & export DISPLAY=:99
 firefox --version
 firefox --headless --screenshot https://www.google.com
