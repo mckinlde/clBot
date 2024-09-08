225db70e88b8d4187ed84fff5e5b5a8c54fb4ec1
Douglas <douglas.e.mckinley@gmail.com>
2024-09-07 17:24:16 -0700

    Update run_seattle_cars.sh

--- run_seattle_cars.sh
+++ run_seattle_cars.sh
@@ -3,6 +3,11 @@
 # Set up logging for the script
 log_file="/home/ec2-user/seattle_cars_error.log"
 
-# Run the Python script with the area set as a command-line argument (e.g., 5 for the fifth area set)
-# Replace 5 with the area set number you want to use for each instance
-python3 clBot/seattle_cars.py 5 >> "$log_file" 2>&1
+# Check if an area set number is provided
+if [ -z "$1" ]; then
+  echo "Error: No area set number provided." >> "$log_file"
+  exit 1
+fi
+
+# Run the Python script with the area set number passed as a command-line argument
+python3 clBot/seattle_cars.py "$1" >> "$log_file" 2>&1
