1fab7f7b5b826373c0c74d3a8841d467eb5734c3
Douglas <douglas.e.mckinley@gmail.com>
2024-09-07 17:06:27 -0700

    Update and rename setup_firefox.sh to setup_firefox_and_geckodriver.sh
    cd clBot/
    git pull origin main
    cd ../
    chmod +x ~/clBot/setup_firefox.sh
    ./clBot/setup_firefox.sh

--- setup_firefox_and_geckodriver.sh
+++ setup_firefox_and_geckodriver.sh
@@ -6,5 +6,15 @@ sudo yum install -y mesa-libGL
 export MOZ_HEADLESS=1
 sudo yum install -y xorg-x11-server-Xvfb
 Xvfb :99 & export DISPLAY=:99
+wget https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.33.0-linux64.tar.gz
+sudo mv geckodriver /usr/local/bin/
 firefox --version
 firefox --headless --screenshot https://www.google.com
+
+# from ec2-user% you can run this with:
+# cd clBot/
+# git pull origin main
+# cd ../
+# chmod +x ~/clBot/setup_firefox.sh
+# ./clBot/setup_firefox.sh
